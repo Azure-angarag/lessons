9dc40d6cf2368b8d009770593278e53783628e7e
Evening <evening@mstars-lab3-01s-iMac.local>
2022-12-05 17:35:52 +0800

    12.5 array lesson

---
+++ js/JS/array.js
@@ -0,0 +1,7 @@
+let  itCompanies = ["Facebook" ,"Google" , "Microsoft" , "Apple", "IBM", "Oracle", "Amazon"];
+
+console.log(itCompanies.length);
+
+for(let i = 0; i < itCompanies.length; i++){
+    console.log(itCompanies[i],"urt ni",itCompanies[i].length)
+} 
