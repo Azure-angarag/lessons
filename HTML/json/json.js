d96d00cdb8ec85ce03d004224f91604341a78417
Evening <evening@mstars-lab3-01s-iMac.local>
2022-12-28 17:51:32 +0800

    E-commerce bagshiinhaar

--- HTML/json/json.js
+++ HTML/json/json.js
@@ -1,10 +1,14 @@
 console.log("ECOMMERCE");
 // All variables and DOM
 const productList = document.querySelector(".productList");
+const categoryList = document.querySelector(".categoryList");
 const cartList = document.querySelector(".cartList");
 const productsEvent = document.querySelector(".productsEvent");
 const cartCount = document.querySelector(".cartCount");
+const cartPrice = document.querySelector(".cartPrice");
+
 let allProducts = [];
+let allCategories = [];
 let cartProducts = [];
 
 const displayProduct = () => {
@@ -23,16 +27,44 @@ const displayProduct = () => {
                   <h5 class="card-title">
                     ${product.title}
                   </h5>
+                  <h5 class="text-primary">$${product.price}</h5>
                   <p class="card-text text-truncate">
                     ${product.description}
                   </p>
-                  <button onclick="addCart(${idx})"  class="btn btn-primary addCart" >Add card</button>
+                  <button onclick="addCart(${product.id})"  class="btn btn-primary addCart" >Add card</button>
                 </div>
               </div>
             </div>
         `;
     productList.innerHTML += productItem;
   });
+};
+
+const displayCategory = () => {
+  categoryList.innerHTML = "";
+  allCategories.forEach((category) => {
+    const categoryItem = `
+      <button onclick="getCategoryProduct('${category}')" class="btn btn-primary">${category}</button>
+    `;
+    categoryList.innerHTML += categoryItem;
+  });
+};
+
+const getCategories = async () => {
+  const response = await fetch("https://dummyjson.com/products/categories");
+  const data = await response.json();
+  allCategories = data;
+  displayCategory();
+};
+
+const getCategoryProduct = async (category) => {
+  console.log(category);
+  const response = await fetch(
+    `https://dummyjson.com/products/category/${category}`
+  );
+  const data = await response.json();
+  allProducts = data.products;
+  displayProduct();
 };
 
 const getProducts = async () => {
@@ -42,13 +74,31 @@ const getProducts = async () => {
   displayProduct();
   displayCart();
 };
-
+getCategories();
 getProducts();
 
-const addCart = (idx) => {
-  cartProducts.push(allProducts[idx]);
+const addCart = (productId) => {
+  const findIdx = cartProducts.findIndex((item) => item.id === productId);
+  if (findIdx > -1) {
+    //ene baraa cartProducts array dotor bval nemehgui harin baraanii too hemjee nemne
+    cartProducts[findIdx].count += 1;
+  } else {
+    //bhgui bol baraag nemne
+    const findIndex = allProducts.findIndex((item) => item.id === productId);
+
+    const newBaraa = { count: 1, ...allProducts[findIndex] };
+    cartProducts.push(newBaraa);
+  }
   cartCount.innerText = cartProducts.length;
   displayCart();
+};
+
+const calculateCartPrice = () => {
+  let sumPrice = 0;
+  for (product of cartProducts) {
+    sumPrice = sumPrice + product.price * product.count;
+  }
+  return sumPrice;
 };
 
 const displayCart = () => {
@@ -71,13 +121,13 @@ const displayCart = () => {
               <h5 class="text-primary">${product.title}</h5>
               <h6 style="color: #9e9e9e">Color: white</h6>
               <div class="d-flex align-items-center">
-                <p class="fw-bold mb-0 me-5 pe-3">${product.price}</p>
+                <p class="fw-bold mb-0 me-5 pe-3">$${product.price}</p>
                 <div class="">
                   <input
                     class="form-control"
                     min="0"
                     id="quantity"
-                    value="1"
+                    value="${product.count}"
                     type="number"
                   />
                 </div>
@@ -86,4 +136,6 @@ const displayCart = () => {
         </div>`;
     cartList.innerHTML += cartItem;
   }
+  const totalCartPrice = calculateCartPrice();
+  cartPrice.innerText = `$${totalCartPrice}`;
 };
